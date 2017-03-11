3d13c178fe5abe6901a9f27bc7dad826b01a9004
bacchi.rafael <bacchi.rafael@gmail.com>
2017-03-11 11:07:08 +0100

    Icono de cargando en boton cuando formulario ajax esta enviando informacion

--- app/helpers/pop_admin/form_helper.rb
+++ app/helpers/pop_admin/form_helper.rb
@@ -19,17 +19,20 @@ module PopAdmin::FormHelper
   end
 
   def form_buttons(model, options = {})
-    options.reverse_merge!(icons: false)
+    options.reverse_merge!(icons: false, save_label: t('action.save'),
+      cancel_label: t('action.cancel'))
     icon_style = options[:icons] ? "btn-icon icon-left" : ""
+    disabled_html = "<i class='fa fa-refresh fa-spin'></i> #{options[:save_label]}"
     content_tag('div', class: 'row') do
       content_tag('div', class: 'col-md-12') do
         content_tag('div', class: 'form-group pop-form-actions') do
-          button_tag(class: "btn btn-primary #{icon_style}") do
-            concat(options[:save_label] || t('action.save'))
+          button_tag(class: "btn btn-primary #{icon_style}", data: {
+            disable_with: disabled_html }) do
+            concat(options[:save_label])
             concat(content_tag('i', '', class: "entypo-check")) if options[:icons]
           end +
           link_to(model, class: "btn btn-default #{icon_style}") do
-            concat(options[:cancel_label] || t('action.cancel'))
+            concat(options[:cancel_label])
             concat(content_tag('i', '', class: 'entypo-cancel')) if options[:icons]
           end
         end
